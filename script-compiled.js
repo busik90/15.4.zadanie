64edb0653eb3666818bcf2fdfa4597f84e52a19b
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-09 13:44:18 +0200

    Add scripts

---
+++ script-compiled.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var colors = require('colors');
+
+// Funkcja Zadanie ..
+var zadanie = function zadanie(parametr, content) {
+      var color = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'white';
+
+      colors.setTheme({
+            custom: color
+      });
+
+      var zadanieNr = 'Zadanie ' + parametr + ':';
+      var contentToWrite = '';
+
+      for (var log in content) {
+            contentToWrite += content[log] + '\n';
+      };
+
+      console.log(zadanieNr.blue + '\n' + contentToWrite.custom);
+};
+
+// Zadanie pierwsze
+var hello = 'Hello',
+    world = 'World';
+
+zadanie('pierwsze', [hello + ' ' + world + '!']);
+
+// Zadanie drugie
+var multiply = function multiply(a, b) {
+      return a * b;
+};
+
+zadanie('drugie', ['2 * 5 = ' + multiply(2, 5), '6 * 6 = ' + multiply(6, 6)]);
+
+// Zadanie trzecie
+var average = function average() {
+      for (var _len = arguments.length, numbers = Array(_len), _key = 0; _key < _len; _key++) {
+            numbers[_key] = arguments[_key];
+      }
+
+      var result = 0;
+      numbers.forEach(function (number) {
+            result += number;
+            return result;
+      });
+      return result / numbers.length;
+};
+
+zadanie('trzecie', [average(1), average(1, 3), average(1, 3, 6, 6)]);
+
+// Zadanie czwarte
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+// Nie wiem jak przekazać tablicę, żeby działało bez zmieniania funkcji
+zadanie('czwarte', [average(grades)], 'red');
+
+// Zadanie piąte
+var oddArray = [1, 4, 'Iwona', false, 'Nowak'],
+    firstName = oddArray[2],
+    lastName = oddArray[4];
+
+
+zadanie('pi\u0105te', [firstName + ' ' + lastName]);
---
+++ script.js
@@ -0,0 +1,52 @@
+const colors = require('colors');
+
+// Funkcja Zadanie ..
+const zadanie = (parametr, content, color = `white`) => {
+      colors.setTheme({
+            custom: color
+      });
+
+      const zadanieNr = `Zadanie ${parametr}:`;
+      let contentToWrite = ``;
+
+      for (let log in content) { contentToWrite += content[log] + `\n`; };
+
+      console.log(`${zadanieNr.blue}\n${contentToWrite.custom}`);
+}
+
+// Zadanie pierwsze
+const hello = 'Hello',
+      world = 'World';
+
+zadanie(`pierwsze`, [`${hello} ${world}!`]);
+
+// Zadanie drugie
+const multiply = (a, b) => a * b;
+
+zadanie(`drugie`, [`2 * 5 = ${multiply(2, 5)}`,
+                   `6 * 6 = ${multiply(6, 6)}`]);
+
+// Zadanie trzecie
+const average = (...numbers) => {
+      let result = 0;
+      numbers.forEach(number => {
+            result += number;
+            return result;
+            }
+      )
+      return result / numbers.length;
+}
+
+zadanie(`trzecie`, [average(1), average(1, 3), average(1, 3, 6, 6)]);
+
+// Zadanie czwarte
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+// Nie wiem jak przekazać tablicę, żeby działało bez zmieniania funkcji
+zadanie(`czwarte`, [average(grades)], `red`);
+
+// Zadanie piąte
+const oddArray = [1, 4, 'Iwona', false, 'Nowak'],
+      [, , firstName, , lastName] = oddArray;
+
+zadanie(`piąte`, [`${firstName} ${lastName}`]);
